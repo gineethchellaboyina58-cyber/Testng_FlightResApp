236427fdcacf3403eba9389bad98c97a3d311bab
npradeepkumar418-star <npradeepkumar418@gmail.com>
2025-12-31 15:57:31 +0530

    New_testCase

---
+++ OrangeHRMtestNG/src/test/java/OrangeHRMTestcases/Practice_test.java
@@ -0,0 +1,12 @@
+package OrangeHRMTestcases;
+
+import org.testng.annotations.Test;
+
+public class Practice_test {
+	@Test
+	public void practicemetho() {
+		System.out.println("Just practice git_hub");
+		
+	}
+
+}
